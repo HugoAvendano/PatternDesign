aa409fa37dd642011c347ea9a34650c6d9ac8118
Hugo Avendaño <hugoavendano2014@gmail.com>
2023-01-02 17:07:42 -0300

    Refactoring buildPayment method

--- src/com/havendan/creational/factory/PaymentFactory.java
+++ src/com/havendan/creational/factory/PaymentFactory.java
@@ -2,13 +2,10 @@ package com.havendan.creational.factory;
 
 public class PaymentFactory {
     public static Payment buildPayment(TypePayment typePayment) throws Exception{
-        switch (typePayment){
-            case CARD_PAYMENT:
-                return new CardPayment();
-            case GOOGLE_PAYMENT:
-                return new GooglePayment();
-            default:
-                throw  new Exception("non-existent form of payment");
-        }
+        return switch (typePayment) {
+            case CARD_PAYMENT -> new CardPayment();
+            case GOOGLE_PAYMENT -> new GooglePayment();
+            default -> throw new Exception("non-existent form of payment");
+        };
     }
 }
